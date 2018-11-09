bca180faa8323f623a4c6da72470798d0275872f
Bno93 <benno.schweikert@googlemail.com>
2018-11-09 13:39:48 +0100

    add try catch for command with logging the error message

--- src/clipboard.ts
+++ src/clipboard.ts
@@ -14,29 +14,54 @@ export function activate(context: ExtensionContext) {
     logger.addMessage('clipboard History activated');
 
     disposableArray.push(commands.registerCommand('clipboard.copy', () => {
-        extCommands.addClipboardItem(window.activeTextEditor);
+        try {
+            extCommands.addClipboardItem(window.activeTextEditor);
+        } catch(error) {
+            logger.addMessage("[ERROR] clipboard.copy => addClipboardItem() " + error.message);
+        }
         commands.executeCommand("editor.action.clipboardCopyAction");
     }));
 
     disposableArray.push(commands.registerCommand('clipboard.cut', () => {
-        extCommands.addClipboardItem(window.activeTextEditor);
+        try {
+            extCommands.addClipboardItem(window.activeTextEditor);
+        } catch (error) {
+            logger.addMessage("[ERROR] clipboard.cut => addClipboardItem() " + error.message);
+        }
         commands.executeCommand("editor.action.clipboardCutAction");
     }));
 
     disposableArray.push(commands.registerCommand('clipboard.paste', () => {
-        commands.executeCommand("editor.action.clipboardPasteAction");
+        try {
+            commands.executeCommand("editor.action.clipboardPasteAction");
+        } catch (error) {
+            logger.addMessage("[ERROR] clipboard.paste => clipboardPasteAction() " + error.message);
+        }
     }));
 
     disposableArray.push(commands.registerCommand('clipboard.pasteFromClipboard', () => {
-        extCommands.pasteFromClipboard();
+        try {
+            extCommands.pasteFromClipboard();
+
+        } catch (error) {
+            logger.addMessage("[ERROR] clipboard.pasteFromClipboard => pasteFromClipboard() " + error.message);
+        }
     }));
 
     disposableArray.push(commands.registerCommand('clipboard.removeFromClipboard', () => {
-        extCommands.removeFromClipboard();
+        try {
+            extCommands.removeFromClipboard();
+        } catch (error) {
+            logger.addMessage("[ERROR] clipboard.removeFromClipboard => removeFromClipboard() " + error.message);
+        }
     }));
 
     disposableArray.push(commands.registerCommand('clipboard.editClipboard', () => {
-        extCommands.editClipboard();
+        try {
+            extCommands.editClipboard();
+        } catch (error) {
+            logger.addMessage("[ERROR] clipboard.editClipboard=> editClipboard() " + error.message);
+        }
     }));
 
     context.subscriptions.concat(disposableArray);
